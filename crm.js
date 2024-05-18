ae8e00f19cfe7507909fd7384b5d66d5becd29be
Jakin Rogel <96803752+JakinRogel@users.noreply.github.com>
2024-05-18 13:55:09 -0700

    Update crm.js

--- crm.js
+++ crm.js
@@ -1,5 +1,3 @@
-// Import jQuery
-document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>');
 
 // Function to handle the delete button click
 function handleDeleteButtonClick(event) {
